0f1a77e642b0df33740c154fc8e6b96830ca1e3b
Epat <kamikadze2185@gmail.com>
2017-08-27 03:44:15 +0200

    up5.1

---
+++ temp/temp.js
@@ -0,0 +1,34 @@
+//obsługa info o komputerze
+const os = require("os");
+const platform = os.platform();
+if (platform == "win32") {
+  var tempPath = os.homedir() + "\\YourHomeBudget\\";
+}else{
+  var tempPath = os.homedir() + "/YourHomeBudget/";
+}
+const filesPath = tempPath;
+
+
+//sprawdź czy folder roboczy programu istnieje w katalogu domowym
+  fs.access(filesPath, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+    if (err) {
+      // neśli nie, stwórz go
+      console.log('no access!');
+      fs.mkdir(filesPath);
+    }
+    else {
+      console.log('can read/write');
+
+    }
+  });
+
+function save(name, data){
+  let path = filesPath + name;
+  fs.writeFile(path, data,  function(err) {
+   if (err) {
+      return console.error(err);
+   }
+
+   console.log("Data written successfully!");
+ });
+}
--- workerJS/main.js
+++ workerJS/main.js
@@ -1,6 +1,5 @@
-// obsługa plików
 const fs = require('fs');
 const electron = require('electron');
 const remote = electron.remote;
 const app = remote.app;
-const {ipcRenderer} = electron; // komunikacja pomiedzy procesami
+const {ipcRenderer} = electron;
