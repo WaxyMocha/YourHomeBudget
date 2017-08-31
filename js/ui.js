2bef00d330dc30e317747a75378fa13ec4446ce5
Epat <kamikadze2185@gmail.com>
2017-08-31 06:54:03 +0200

    up6.2

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,11 @@
-const menuButton = document.getElementById("menuButton"),
-mainMenu =  document.getElementById("mainMenu");
+const menuButton = document.getElementById('menuButton');
+const mainMenu =  document.getElementById('mainMenu');
+
 
 menuButton.addEventListener('click', (e) => {
-  if (mainMenu.style.left == "0px") {
-    mainMenu.style.left = "-100vw";    
+  if (mainMenu.style.left == '0px') {
+    mainMenu.style.left = -'100vw';
   } else {
-    mainMenu.style.left = "0px";
+    mainMenu.style.left = '0px';
   }
-})
+});
