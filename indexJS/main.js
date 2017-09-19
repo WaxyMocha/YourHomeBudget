b22921f26a69f67325cd63892354678be03e7989
Epat9 <kamikadze2185@gmail.com>
2017-09-19 18:49:12 +0200

    PreAlpha 0.0.1 up5

--- indexJS/main.js
+++ indexJS/main.js
@@ -1,11 +1,25 @@
 /*jshint esversion: 6 */
 
+function addIncome(incomeType, name, desc, amount, cat) {
+  let tmpArr;
+  if (incomeType == 'profit') {
+    tmpArr = profits;
+  } else if (incomeType == 'loss') {
+    tmpArr = loss;
+  } else {
+    console.log('Invalid incomeType');
+    return;
+  }
+
+  let tmpObj = {};
+}
+
 let profits = [];
 let loss = [];
 let budget = {
   ID: 0,
   monthsIDs: [
-    0,
+    { id: 0, path: 'path' },
   ],
   lastMonthID: 0,
 };
