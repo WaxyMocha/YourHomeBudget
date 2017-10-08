84d7186f78cb6f8ca196ed15cd5c1bb8fb637bfe
Epat9 <kamikadze2185@gmail.com>
2017-10-08 15:32:17 +0200

    PreAlpha 0.0.1 up 10

--- indexJS/main.js
+++ indexJS/main.js
@@ -1,29 +1,40 @@
 /*jshint esversion: 6 */
 
+let stop = true;
+let config = {
+  // lastBudget: 'budgets/budget-0/',
+};
 let profits = [];
 let loss = [];
 let budget = {
-  id: 0,
-  path: `budgets/budget-0/`,
-  monthsID: [
-    {
-      id: 0,
-      path: `budgets/budget-0/month-0/`,
-    },
-  ],
-  lastMonthID: 0,
+  // id: 0,
+  // path: `budgets/budget-0/`,
+  // monthsID: [
+  //   {
+  //     id: 0,
+  //     path: `budgets/budget-0/month-0/`,
+  //   },
+  // ],
+  // lastMonthID: 0,
 };
 
 let month = {
-  amount: 0,
-  id: 0,
-  name: '',
-  descr: '',
-  profits: '',
-  loss: '',
+  // amount: 0,
+  // id: 0,
+  // name: '',
+  // descr: '',
+  // profits: '',
+  // loss: '',
 };
 
-function saveMonth() {
+function saveAll() {
+
+  send({
+    task: 'save',
+    data: { name: 'config', data: config },
+    path: '',
+    name: 'config.json',
+  });
 
   send({
     task: 'save',
@@ -56,11 +67,73 @@ function saveMonth() {
 }
 
 function checkArg(arg) {
-  console.log(arg);
 
-  // if (arg.task = 'read') {
-  //
-  // }
+  if (arg.task == 'read') {
+    let name = arg.data.name;
+    let data = arg.data.data;
+    if (name == 'budget') {
+      budget = data;
+    } else if (name == 'month') {
+      month = data;
+    } else if (name == 'profits') {
+      profits = data;
+    } else if (name == 'loss') {
+      loss = data;
+    } else if (name == 'config') {
+      config = data;
+    } else {
+      console.log('invalid name');
+    }
+
+    stop = false;
+  }
+}
+
+function wait() {
+  do {
+    console.log('xd');
+    if (stop == false) {
+      stop = true;
+      console.log('lol');
+      return;
+    }
+  } while (true);
+}
+
+function startup(arg) {
+
+  if (arg == 'config') {
+    send({
+      task: 'read',
+      path: '',
+      name: 'config.json',
+    });
+  } else if (arg == 'budget') {
+    send({
+      task: 'read',
+      path: config.lastBudget,
+      name: 'budget.json',
+    });
+  } else if (arg == 'month') {
+    send({
+      task: 'read',
+      path: budget.monthsID[lastMonthID].path,
+      name: 'month.json',
+    });
+  } else if (arg == 'profits') {
+    send({
+      task: 'read',
+      path: budget.monthsID[lastMonthID].path,
+      name: 'profits.json',
+    });
+  } else if (arg == 'loss') {
+    send({
+      task: 'read',
+      path: budget.monthsID[lastMonthID].path,
+      name: 'loss.json',
+    });
+  }
+
 }
 
 function addIncome(incomeType, name, desc, amount, cat) {
