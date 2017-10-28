9546782101de0649e671d00836dcab00f8d74365
Epat <kamikadze2185@gmail.com>
2017-10-29 01:55:57 +0200

    up 0

--- indexJS/requires.js
+++
@@ -1,6 +0,0 @@
-/* jshint esversion: 6 */
-const electron = require('electron');
-const remote = electron.remote;
-const app = remote.app;
-const { ipcRenderer } = electron;
-const electronTitlebar = require('electron-titlebar');
