03056b09790d796b75bd8a08f50bcbfd35af48e7
Epat <kamikadze2185@gmail.com>
2018-01-26 20:25:08 +0100

    small fixes

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -185,7 +185,6 @@ function updateMonth() {
   * returns date in format: MM:YYYY
   */
 function monthYear(date = new Date()) {
-  date = new Date(date);
   let dateFormatted = `${date.getMonth() + 1}-${date.getFullYear()}`;
   return dateFormatted;
 }
